5524afbf9e90fcc2c9b1c610ed53c1597e9f3d7b
Benny Lin <helloimbenny@gmail.com>
2014-10-15 22:46:17 -0600

    Disable the creation of GridPage superclass

--- html/mysite/code/GridPage.php
+++ html/mysite/code/GridPage.php
@@ -2,7 +2,7 @@
 class GridPage extends Page {
 
 	private static $allowed_children = 'none';
-    private static $can_be_root = false;
+  private static $can_be_root = false;
 
 	private static $db = array(
 		'isRow' => 'Boolean'
@@ -19,6 +19,10 @@ class GridPage extends Page {
 		$rowType = $this->ClassName;
 		return $this->renderWith($rowType);
 	}
+
+  public function canCreate($Member = null){
+    return ($this->ClassName != "GridPage");
+  }
 }
 
 class GridPage_Controller extends Page_Controller {
